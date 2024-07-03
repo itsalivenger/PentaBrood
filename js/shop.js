241dedb3e487dd9707a3aeb28b33adb80863a651
itsalivenger <alihoussa16@gmail.com>
2024-07-03 22:24:56 +0100

    first commit

--- js/shop.js
+++ js/shop.js
@@ -1,16 +1,16 @@
 const url = 'https://penta-brood-server.vercel.app/products';
 
 // Sample product template
-const productTemplate = ({name, price, ImgSrc}) => `
+const productTemplate = ({ name, price, ImgSrc }) => `
   <div class="col-lg-6 col-md-6 item-entry mb-4">
-                <a href="#" class="product-item md-height bg-gray d-block">
-                  <img src="${ImgSrc}" alt="Image" class="img-fluid">
-                </a>
-                <h2 class="item-title"><a href="#">${name}</a></h2>
-                <strong class="item-price">${price} DH</strong>
-            </div>`;
+    <a href="#" class="product-item md-height bg-gray d-block">
+      <img src="${ImgSrc}" alt="Image" class="img-fluid">
+    </a>
+    <h2 class="item-title"><a href="#">${name}</a></h2>
+    <strong class="item-price">${price} DH</strong>
+  </div>`;
 
-onload = async () => {
+window.onload = async () => {
   const products = await fetchProducts();
   console.log(products);
 
