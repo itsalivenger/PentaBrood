b055f000a68ef1fd9e619efb7e2c02b2977c5c12
itsalivenger <alihoussa16@gmail.com>
2024-07-03 23:06:54 +0100

    first commit

--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,4 @@
-const url = 'https://penta-brood-server.vercel.app';
+const url = 'https://penta-brood-server.vercel.app/';
 
 // Sample product template
 const productTemplate = ({ name, price, ImgSrc }) => `
