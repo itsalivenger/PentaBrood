11ab99ba3364b1ebac636ee80a06c89a3feaf382
itsalivenger <alihoussa16@gmail.com>
2024-07-04 19:26:38 +0100

    first commit

--- js/shop.js
+++ js/shop.js
@@ -4,7 +4,7 @@ const url = 'https://penta-brood-server.vercel.app/products/shop';
 // Sample product template
 const productTemplate = ({_id, name, price, imageSrc }) => `
   <div class="col-lg-6 col-md-6 item-entry mb-4">
-    <a href=${`/shop-single.html?id=${_id}`} class="product-item md-height bg-gray d-block">
+    <a href=${`/PentaBrood/shop-single.html?id=${_id}`} class="product-item md-height bg-gray d-block">
       <img src="${imageSrc}" alt="Image" class="img-fluid">
     </a>
     <h2 class="item-title"><a href="#">${name}</a></h2>
