baa33f6526d9c06dd8b5118c549567667de9b800
itsalivenger <alihoussa16@gmail.com>
2024-07-10 13:51:55 +0100

    logging the thankyouhtml

---
+++ js/thankyou.js
@@ -0,0 +1,7 @@
+// const url = 'https://penta-brood-server.vercel.app/success';
+const url = 'http://localhost:3000/success';
+
+addEventListener('DOMContentLoaded', async () => {
+    const req = await fetch(url).then(res => res.json());
+    console.log(req)
+})
