c4d27b4f4ab4c6e91335a0960a77449a5a98d86e
itsalivenger <alihoussa16@gmail.com>
2024-07-10 16:47:29 +0100

    fixed chi l3ibat b7al checkout o chihajat hakak o add product

--- product-admin-master/js/addProd.js
+++ product-admin-master/js/addProd.js
@@ -3,12 +3,12 @@ const url = 'https://penta-brood-server.vercel.app/products';
 
 
 function checkForInputs() {
-    const name = document.getElementById('name').value || 2;
-    const desc = document.getElementById('description').value || 2;
-    const price = document.getElementById('price').value || 2;
-    const sizes = document.getElementById('quantity').value || 2;
+    const name = document.getElementById('name').value;
+    const desc = document.getElementById('description').value;
+    const price = document.getElementById('price').value;
+    const sizes = document.getElementById('quantity').value;
     const imageSrc = document.getElementById('imageSrc').value;
-    const category = document.getElementById('category').value || 2;
+    const category = document.getElementById('category').value;
     const timer = 0
 
     if (name && description && price && quantity && category) {
@@ -19,7 +19,7 @@ function checkForInputs() {
         if(timer >= 3){
             alert('a3chirey ghi chof kidir meana... rah baqi khassek chi data 3la produit madkheltihach');
         }else{
-            alert('a3chirey rah baqi madkhelti ga3 data 3la dak l produit...');
+            alert('a3chirey rah baqi madkhelti ga3 data 3la dak l produit... Kifach biti produit idkhel o nta mazal madkhelti ta ahaj eliha  echirey !!! RAH A EHCIRYE MAKAL#BOCH HNA A ECHIREY DKHEL L DAKCHI INFO DYALO BACH NDKHLO LIK L PRODUIT A ECHIREY WAH !');
         }
     }
 }
@@ -29,5 +29,23 @@ function addProduct(product) {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(product)
-    }).then((res) => res.json()).then((data) => console.log(data))
+    }).then((res) => res.json()).then((data) => {
+        console.log(data);
+        if(data.error){
+            console.error(error);
+            alert('Error adding product');
+        }else{
+            clearInputs();
+            alert('Product added successfully ! lay3tik se7a a echirey t ajoutaw les produits o dakchi hwaaaa hadak. la bghiti checkih ser l shop checki');
+        }
+    })
+}
+
+function clearInputs() {
+    document.getElementById('name').value = '';
+    document.getElementById('description').value = '';
+    document.getElementById('price').value = '';
+    document.getElementById('quantity').value = '';
+    document.getElementById('imageSrc').value = '';
+    document.getElementById('category').value = '';
 }
