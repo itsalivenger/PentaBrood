0ff63c74139c05383147fc22efcac0d5bbfe5860
itsalivenger <alihoussa16@gmail.com>
2024-07-14 18:30:26 +0100

    fixed the category filter

--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,6 @@
 // const url = 'http://localhost:3000/products/shop';
 const url = 'https://penta-brood-server.vercel.app/products/shop';
+let gProducts = []
 
 // Sample product template
 const productTemplate = ({_id, name, price, imageSrc }) => `
@@ -14,16 +15,99 @@ const productTemplate = ({_id, name, price, imageSrc }) => `
   document.addEventListener("DOMContentLoaded", async () => {
     try {
       const {txt, products} = await fetchProducts();
-  
-      const shopItemsContainer = document.querySelector("#productsContainer");
-      products.forEach(product => {
-        shopItemsContainer.innerHTML += productTemplate(product);
-      });
+      gProducts = products
+      resetFilter()
     } catch (error) {
       console.error('Error loading products:', error);
     }
+    
+    // filter by category
+    const categories = document.querySelectorAll('.categories');
+
+    for (let i = 0; i < categories.length; i++) {
+      categories[i].addEventListener('click', ()=> sortByCategory(categories[i].dataset.value))
+    }
+
+    // filter by price
+    const priceSlider = document.querySelector('#amount');
+    const [, min, max] = priceSlider.value.match(/\$?(\d+)\s*-\s*\$?(\d+)/);
+    document.querySelector('#filterByPriceBtn').addEventListener('click', ()=> sortByPrice(min, max))
+
+    // reset filter
+    const resetFilterBtn = document.getElementById('resetFilter');
+    resetFilterBtn.addEventListener('click', ()=> resetFilter()) 
+
+    // set number of items by filter
+    countProductsByCategory();
   });
-  
+
+function sortByCategory(category) {
+  const shopItemsContainer = document.querySelector("#productsContainer");
+  shopItemsContainer.innerHTML = '';
+  gProducts.forEach((product)=>{
+    if(product.category === category || category == 'All'){
+      shopItemsContainer.innerHTML += productTemplate(product);
+    }
+  })
+}
+
+function sortByPrice(priceMin, priceMax) {
+  const shopItemsContainer = document.querySelector("#productsContainer");
+  shopItemsContainer.innerHTML = '';
+  gProducts.forEach((product)=>{
+    console.log(priceMin);
+    if(parseInt(product.price) >= priceMin && parseInt(product.price) <= priceMax){
+      shopItemsContainer.innerHTML += productTemplate(product);
+    }
+  })
+}
+
+function resetFilter() {
+  const shopItemsContainer = document.querySelector("#productsContainer");
+  shopItemsContainer.innerHTML = '';
+  gProducts.forEach((product)=>{
+    shopItemsContainer.innerHTML += productTemplate(product);
+  })
+}
+
+function countProductsByCategory() {
+  // Object to store category counts
+  let categoryCounts = {};
+
+  // Iterate through each product
+  gProducts.forEach(product => {
+      let category = product.category;
+
+      // Increment count for the category
+      if (categoryCounts[category]) {
+          categoryCounts[category]++;
+      } else {
+          categoryCounts[category] = 1;
+      }
+  });
+
+  // Convert categoryCounts object to array of objects with category and number properties
+  const result = Object.keys(categoryCounts).map(category => ({
+      category,
+      number: categoryCounts[category]
+  }));
+
+
+  result.forEach(({category, number}) => {
+    let span = document.querySelector('#' + category + 'Num');
+    span.innerText = `${number}`;
+  });
+
+  // calculating the products count (how many products are there)
+  const productsCount = result.reduce((a, ele) => {
+    return a + ele.number;
+  }, 0)
+  document.querySelector('#AllNum').innerText = productsCount;
+
+  return result;
+}
+
+
 async function fetchProducts() {
   try {
     const response = await fetch(url, {
